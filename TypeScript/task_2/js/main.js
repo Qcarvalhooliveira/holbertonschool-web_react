ce4be6e6926ff440015176a25d2ff02162eb6241
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-04-17 17:54:01 +0200

    Advanced types Part 1

---
+++ TypeScript/task_2/js/main.js
@@ -0,0 +1,36 @@
+var Director = /** @class */ (function () {
+    function Director() {
+    }
+    Director.prototype.workFromHome = function () {
+        return "Working from home";
+    };
+    Director.prototype.getCoffeeBreak = function () {
+        return "Getting a coffee break";
+    };
+    Director.prototype.workDirectorTasks = function () {
+        return "Getting to director tasks";
+    };
+    return Director;
+}());
+var Teacher = /** @class */ (function () {
+    function Teacher() {
+    }
+    Teacher.prototype.workFromHome = function () {
+        return "Cannot work from home";
+    };
+    Teacher.prototype.getCoffeeBreak = function () {
+        return "Cannot have a break";
+    };
+    Teacher.prototype.workTeacherTasks = function () {
+        return "Getting to work";
+    };
+    return Teacher;
+}());
+var createEmployee = function (salary) {
+    if (typeof salary === "number" && salary < 500) {
+        return new Teacher();
+    }
+    else {
+        return new Director();
+    }
+};
