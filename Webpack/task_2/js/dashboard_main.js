322ef3a0ce97c3343ad84d992e0670d1546c5d94
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-04-20 20:17:16 +0200

    update

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -1,22 +1,22 @@
 import $ from 'jquery';
 import _ from 'lodash';
-import '../css/main.css';
 
-$(function() {
-    $('body').append('<div id="logo"></div>');
-    $('body').append('<p>Holberton Dashboard</p>');
-    $('body').append('<p>Dashboard data for the students</p>');
-    const $button = $('<button>Click here to get started</button>');
-    $('body').append($button);
-    $('body').append("<p id='count'></p>");
-    $('body').append("<p>Copyright - Holberton School</p>");
 
-    let count = 0;
+let count = 0;
+function updateCounter() {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+}
 
-    function updateCounter() {
-        count++;
-        $('#count').text(`${count} clicks on the button`);
-    }
+$(document).ready(() => {
+  $('body').append(`
+    <p>Holberton Dashboard</p>
+    <p>Dashboard data for the students</p>
+    <button>Click here to get started</button>
+    <p id='count'></p>
+    <p>Copyright - Holberton School</p>
+  `);
 
-    $('button').on('click', _.debounce(updateCounter, 500));
+  const debouncedUpdateCounter = _.debounce(updateCounter, 500);
+  $('button').click(debouncedUpdateCounter);
 });
--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,27 +1,27 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "production",
-  entry: "./js/dashboard_main.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      }
-    ]
-  },
-  performance: {
-    hints: 'warning',
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000
-  }
-};
+    entry: './js/dashboard_main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
+    }
+}
