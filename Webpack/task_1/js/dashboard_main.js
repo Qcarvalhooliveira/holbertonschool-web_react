7e0e7b5a8b48b051985822ef704ddf5006a022f7
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-04-20 16:18:20 +0200

    Update dashboard_main.js

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,11 +1,9 @@
-// Importações necessárias
-const $ = require('jquery');
+import $ from 'jquery';
 import _ from 'lodash';
 
 $(function() {
     $('body').append('<p>Holberton Dashboard</p>');
     $('body').append('<p>Dashboard data for the students</p>');
-
     const $button = $('<button>Click here to get started</button>');
     $('body').append($button);
     $('body').append("<p id='count'></p>");
@@ -18,5 +16,5 @@ $(function() {
         $('#count').text(`${countClicks} clicks on the button`);
     }
 
-    $('button').on('click', _.debounce(updateCounter, 250));
+    $('button').on('click', _.debounce(updateCounter, 500));
 });
