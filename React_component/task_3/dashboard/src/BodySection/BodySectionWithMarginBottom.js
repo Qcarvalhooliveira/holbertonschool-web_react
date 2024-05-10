55cd23b13545fcb682e3812b3f0ed4a47590d717
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-05-10 14:27:09 +0200

    Update BodySectionWithMarginBottom.js

--- React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import BodySection from './BodySection';
-import './BodySectionWithMarginBottom.css';
+import './BodySection.css';
 
 function BodySectionWithMarginBottom(props) {
     return (
