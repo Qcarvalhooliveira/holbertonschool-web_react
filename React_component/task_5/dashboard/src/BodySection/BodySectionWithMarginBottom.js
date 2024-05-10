4be82f29a167734c6c1dab3d41282c67bcc3503d
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-05-10 15:29:02 +0200

    Declare a pure component

---
+++ React_component/task_5/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import BodySection from './BodySection';
+import './BodySection.css';
+
+function BodySectionWithMarginBottom(props) {
+    return (
+        <div className="bodySectionWithMargin">
+            <BodySection {...props} />
+        </div>
+    );
+}
+
+BodySectionWithMarginBottom.propTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.node
+};
+
+export default BodySectionWithMarginBottom;
