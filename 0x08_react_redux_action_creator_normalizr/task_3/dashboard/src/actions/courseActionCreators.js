35842dcb7289fdeeddc7873d8b2c4a7d334ecaaa
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-06-02 21:45:40 +0200

    Create actions for the course list

---
+++ 0x08_react_redux_action_creator_normalizr/task_3/dashboard/src/actions/courseActionCreators.js
@@ -0,0 +1,11 @@
+import { SELECT_COURSE, UNSELECT_COURSE } from './courseActionTypes';
+
+export const selectCourse = (index) => ({
+  type: SELECT_COURSE,
+  index,
+});
+
+export const unSelectCourse = (index) => ({
+  type: UNSELECT_COURSE,
+  index,
+});
